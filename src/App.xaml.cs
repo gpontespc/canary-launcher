3b8993d982504302c92d3c00bc69cfbb1d992d37
Guilherme de Pontes <46386501+gpontespc@users.noreply.github.com>
2025-10-29 22:44:13 -0300

    Ensure TLS 1.2 handshake

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -3,22 +3,25 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using System.Windows;
 
 namespace CanaryLauncherUpdate
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public class Program
-	{
-		[STAThread]
-		public static void Main(string[] args)
-		{
-			App app = new App();
-			app.InitializeComponent();
-			app.Run();
-		}
-	}
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public class Program
+  {
+    [STAThread]
+    public static void Main(string[] args)
+    {
+      ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls12;
+
+      App app = new App();
+      app.InitializeComponent();
+      app.Run();
+    }
+  }
 }
